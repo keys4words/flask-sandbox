d885a1ab4a9b2d452a5c50b44835f7e78c04176a
keys4words <keys4words@gmail.com>
2020-08-08 15:49:48 +0300

    add migration to mydbpractice

--- mydbpractice/mydbpracticeapp/app.py
+++ mydbpractice/mydbpracticeapp/app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -11,6 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'da
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+Migrate(app, db)
 
 
 #### Models
@@ -19,10 +21,12 @@ class Puppy(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     age = db.Column(db.Integer)
+    breed = db.Column(db.Text)
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, breed):
         self.name = name
         self.age = age
+        self.breed = breed
 
     def __repr__(self):
         return f'<Puppy {self.name} is {self.age} years old>'
