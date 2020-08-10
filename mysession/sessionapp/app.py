71bdcb763c12987d6f3927c4c4ab289f0d0f574f
keys4words <keys4words@gmail.com>
2020-08-10 12:57:03 +0300

    start authorization in mysession

--- mysession/sessionapp/app.py
+++ mysession/sessionapp/app.py
@@ -1,8 +1,15 @@
 import os
 from flask import Flask, redirect, render_template, url_for, make_response, request, session
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -13,24 +20,46 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 Migrate(app, db)
+login = LoginManager(app)
+
+
+
+#### Forms
+class LoginForm(FlaskForm):
+    login = StringField('Enter your login', validators=[DataRequired()])
+    email = StringField('Enter your email', validators=[DataRequired()])
+    password = PasswordField('Enter your password', validators=[DataRequired()])
+    submit = SubmitField('Login')
 
 
 #### Models
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    # pass_hash = db.Column(db.String(128))
+    pass_hash = db.Column(db.String(128))
 
-    def __init__(self, login, email):
+    def __init__(self, login, email, password):
         self.login = login
         self.email = email
+        self.pass_hash = self.set_pass(password)
+
+    def set_pass(self, password):
+        self.pass_hash = generate_password_hash(password)
+
+    def check_pass(self, password):
+        return check_password_hash(self.pass_hash, password)
 
     def __repr__(self):
         return f'<User {self.login} with email {self.email}>'
 
-
+#### Views
 @app.route('/', methods=['GET', 'POST'])
 def index():
     content = 'No session'
@@ -51,22 +80,34 @@ def index():
 #     cookie_var = request.cookies.get('cookie_key')
 #     return '<h2>Cookie: ' + cookie_var + '</h2>'
 
-@app.route('/setsession', methods=['GET', 'POST'])
-def setsession():
-    if request.method == 'POST':
-        session['username'] = request.form['username']
-        return redirect(url_for('index'))
-    return '''
-        <form action="" method="post">
-        <p><input type=text name=username></p>
-        <p><input type=submit value=Login></p>
-        </form>
-        '''
+# @app.route('/setsession', methods=['GET', 'POST'])
+# def setsession():
+#     if request.method == 'POST':
+#         session['username'] = request.form['username']
+#         return redirect(url_for('index'))
+#     return '''
+#         <form action="" method="post">
+#         <p><input type=text name=username></p>
+#         <p><input type=submit value=Login></p>
+#         </form>
+#         '''
 
-@app.route('/unsetsession')
-def unsetsession():
-    session.pop('username', None)
-    return redirect(url_for('index'))
+# @app.route('/unsetsession')
+# def unsetsession():
+#     session.pop('username', None)
+#     return redirect(url_for('index'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm(csrf_enabled=True)
+    if form.validate_on_submit():
+        flash(f'Hello, {form.login}')
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
