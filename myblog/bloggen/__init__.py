cffc617134545ad8d18c373525ef6ef45e23e24e
keys4words <keys4words@gmail.com>
2020-08-18 17:12:16 +0300

    finished myblog

--- myblog/bloggen/__init__.py
+++ myblog/bloggen/__init__.py
@@ -21,6 +21,10 @@ login_manager.login_view = 'users.login'
 
 from bloggen.core.views import core
 from bloggen.error_pages.handlers import error_pages
+from bloggen.users.views import users
+from bloggen.posts.views import blog_posts
 
 app.register_blueprint(core)
 app.register_blueprint(error_pages)
+app.register_blueprint(users)
+app.register_blueprint(blog_posts)
--- myblog/bloggen/users/forms.py
+++ myblog/bloggen/users/forms.py
@@ -35,3 +35,11 @@ class UpdateUserForm(FlaskForm):
     username = StringField('UserName', validators=[DataRequired()])
     picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
+
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Your email has been registered already!')
+
+    def check_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Your username has been registered already!')
