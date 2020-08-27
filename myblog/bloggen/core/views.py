4407d1656bdcd28f235b18e87eacbc0f271ea875
keys4words <keys4words@gmail.com>
2020-08-27 10:23:20 +0300

    add count quantities to index

--- myblog/bloggen/core/views.py
+++ myblog/bloggen/core/views.py
@@ -1,14 +1,17 @@
 from flask import render_template, request, Blueprint
-from bloggen.models import Blogpost
+from bloggen.models import Blogpost, User, Postcategory
 
 core = Blueprint('core', __name__)
 
 @core.route('/')
 def index():
     page = request.args.get('page', 1, type=int)
+    qty_posts = Blogpost.query.count()
+    qty_cats = Postcategory.query.count()
+    qty_users = User.query.count()
     # blog_posts = Blogpost.query.order_by(Blogpost.date.desc()).limit(10).paginate(page=page, per_page=5)
     blog_posts = Blogpost.query.order_by(Blogpost.date.desc()).paginate(page=page, per_page=5)
-    return render_template('index.html', blog_posts=blog_posts)
+    return render_template('index.html', blog_posts=blog_posts, qty_posts=qty_posts, qty_cats=qty_cats, qty_users=qty_users)
 
 
 @core.route('/about')
