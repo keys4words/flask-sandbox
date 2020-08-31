0547827ea9abcb51c8f70f8c99e542ea509f4d17
keys4words <keys4words@gmail.com>
2020-08-31 11:09:20 +0300

    fix login in modals app

--- modals/modal_app/__init__.py
+++ modals/modal_app/__init__.py
@@ -6,7 +6,7 @@ from wtforms import StringField, SubmitField, TextAreaField, PasswordField, Bool
 from wtforms.validators import DataRequired, Length, EqualTo
 from flask_login import login_user, current_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
-from passlib.hash import sha256_crypt
+from functools import wraps
 import gc
 from MySQLdb import escape_string as thwart
 
@@ -16,12 +16,25 @@ TOPIC_DICT = Content()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mySecretKey'
 
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to be login first')
+            return redirect(url_for('login_page'))
+    return wrap
+
+
 @app.route('/')
 def homepage():
     return render_template("index.html")
 
 
 @app.route('/dashboard/')
+@login_required
 def dashboard():
     return render_template("dashboard.html", TOPIC_DICT=TOPIC_DICT)
 
@@ -39,22 +52,6 @@ def slashboard():
 	    return render_template("500.html", error = str(e))
 
 
-@app.route('/login/', methods=["GET","POST"])
-def login_page():
-    error = ''
-    try:
-        if request.method == "POST":
-            attempted_username = request.form['username']
-            attempted_password = request.form['password']
-            if attempted_username == "admin" and attempted_password == "password":
-                return redirect(url_for('dashboard'))
-            else:
-                error = "Invalid credentials. Try Again."
-        return render_template("login.html", error = error)
-    except Exception as e:
-        # flash(e)
-        return render_template("login.html", error = error)
-
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', [Length(min=4, max=20)])
@@ -68,18 +65,53 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Register')
 
 
+class LoginForm(FlaskForm):
+    username = StringField('Username', [Length(min=4, max=20)])
+    password = PasswordField('Password', [DataRequired()])
+    submit = SubmitField('Login')
+
+
+
+@app.route('/login/', methods=["GET","POST"])
+def login_page():
+    error = ''
+    form = LoginForm()
+    try:
+        c, conn = connection()
+        if request.method == "POST":
+            data = c.execute("SELECT * FROM users WHERE username = (%s)", (thwart(request.form['username']), ))
+            data = c.fetchone()[2]
+            if check_password_hash(data ,request.form['password']):
+                session['logged_in'] = True
+                session['username'] = request.form['username']
+                flash("You are now logged in")
+                return redirect(url_for("dashboard"))
+            else:
+                error = "Invalid credentials, try again."
+        gc.collect()
+        return render_template("login.html", error=error, form=form)
+
+    except Exception as e:
+        #flash(e)
+        error = "Invalid credentials, try again."
+        return render_template("login.html", error = error)  
+
+
+
+
+
 @app.route('/register/', methods=["GET","POST"])
 def register_page():
     try:
         form = RegistrationForm(request.form)
-
         if request.method == "POST" and form.validate_on_submit():
             username  = form.username.data
             email = form.email.data
-            password = sha256_crypt.encrypt((str(form.password.data)))
+            password = generate_password_hash(form.password.data)
+            print(password)
             c, conn = connection()
 
-            x = c.execute("SELECT * FROM users WHERE username = (%s)", (thwart(username)))
+            x = c.execute("SELECT * FROM users WHERE username = (%s)", (thwart(username), ))
             if int(x) > 0:
                 flash("That username is already taken, please choose another")
                 return render_template("register.html", form=form)
@@ -92,10 +124,18 @@ def register_page():
                 c.close()
                 conn.close()
                 gc.collect()
-
                 session['logged_in'] = True
                 session['username'] = username
                 return redirect(url_for('dashboard'))
         return render_template("register.html", form=form)
     except Exception as e:
         return(str(e))
+
+
+@app.route('/logout/')
+@login_required
+def logout():
+    session.clear()
+    flash('You have been logged out!')
+    gc.collect()
+    return redirect(url_for('homepage'))
