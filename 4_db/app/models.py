9a24edf0bf6f81560cd22b0f764bd3c43679ee5c
keys4words <keys4words@gmail.com>
2020-09-17 17:23:09 +0300

    finished 4_db app

--- 4_db/app/models.py
+++ 4_db/app/models.py
@@ -15,4 +15,10 @@ class User(db.Model):
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f"<Post #{self.id}: {self.body}"
     
