eee6d88f5eb355001f88444d180559461a48674b
keys4words <keys4words@gmail.com>
2020-09-20 23:54:36 +0300

    start 6_authapp

---
+++ 6_authority/app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'myVerySecretKey'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'authapp.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+# login = LoginManager(app)
+# login.login_view = 'login'
+
+from app import views, models
---
+++ 6_authority/app/models.py
@@ -0,0 +1,12 @@
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password = db.Column(db.String(128))
+
+    def __repr__(self):
+        return f"<User #{self.id}: {self.username}"
