de81b97db6ba01f7671266678b356e2a52f5fb88
keys4words <keys4words@gmail.com>
2020-09-29 12:20:32 +0300

    start 7_pirple app

---
+++ 7_pirple/demo.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/football', methods=['GET'])
+def football():
+    return render_template('football.html')
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
