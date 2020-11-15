b9b579cda88a27605ded3c17ecc3cf5660f41752
keys4words <keys4words@gmail.com>
2020-11-15 19:04:33 +0300

    start flask_admin app

---
+++ flask_admin_app/app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_login import UserMixin
+from flask_admin.contrib.sqla import ModelView
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'mySecretKey'
+
+db = SQLAlchemy(app)
+
+
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+
+
+admin = Admin(app)
+admin.add_view(ModelView(User, db.session))
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
