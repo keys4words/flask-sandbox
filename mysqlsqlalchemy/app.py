2c803e3a5324608e7c5a41b1b04f71d7ebfc54a4
keys4words <keys4words@gmail.com>
2020-12-15 10:12:00 +0300

    finished mysqlsqlalchemy app

--- mysqlsqlalchemy/app.py
+++ mysqlsqlalchemy/app.py
@@ -20,8 +20,28 @@ class Member(db.Model):
     email = db.Column(db.String(50))
     join_date = db.Column(db.DateTime)
 
+    orders = db.relationship('Order', backref='member', lazy='dynamic')
+    coursers = db.relationship('Course', secondary='user_courses', backref='member', lazy='dynamic')
+
     def __repr__(self):
         return f'<Member: {self.username}>'
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer)
+    member_id = db.Column(db.Integer, db.ForeignKey('member.id'))
+
+    def __repr__(self):
+        return f'<Order #{self.id} ${self.price}>'
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+db.Table('user_courses', 
+    db.Column('member_id', db.Integer, db.ForeignKey('member.id')),
+    db.Column('course_id', db.Integer, db.ForeignKey('course.id'))
+)
 
 
 
