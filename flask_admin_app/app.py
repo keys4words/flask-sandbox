9bb55203d9ae260d4bc4541749e2edc45c04761b
keys4words <keys4words@gmail.com>
2020-12-29 16:36:41 +0300

    add upload & customizing to admin

--- flask_admin_app/app.py
+++ flask_admin_app/app.py
@@ -1,28 +1,70 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_admin import Admin
+from flask_admin import Admin, BaseView, expose
 from flask_admin.contrib.sqla import ModelView
-import os
+from werkzeug.security import generate_password_hash
+from flask_admin.contrib.fileadmin import FileAdmin
+from os.path import dirname, join
 
-
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'mySecretKey'
+app.config.from_object('config')
 
 db = SQLAlchemy(app)
-
-admin = Admin(app)
+admin = Admin(app, template_mode='bootstrap3')
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
+    username = db.Column(db.String(128))
+    password = db.Column(db.String(256))
+    age = db.Column(db.Integer)
+    birthday = db.Column(db.DateTime)
+    comments = db.relationship('Comment', backref='user', lazy='dynamic')
 
-admin.add_view(ModelView(User, db.session))
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    comment_text = db.Column(db.String(200))
+    usr_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'<Comment {self.id}>'
+
+class CommentView(ModelView):
+    create_modal = True
+
+
+class UserView(ModelView):
+    # column_exclude_list = ['password']
+    column_display_pk = True
+    can_create = True
+    can_edit = True
+    can_delete = False
+    can_export = True
+
+    def on_model_change(self, form, model, is_created):
+        # model.password = 'you-dont-see-real-password'
+        model.password = generate_password_hash(model.password, method='sha256')
+
+    inline_models = [Comment]
+
+
+class NotificationsView(BaseView):
+    @expose('/')
+    def index(self):
+        return self.render('admin/notification.html')
+
+
+admin.add_view(UserView(User, db.session))
+admin.add_view(CommentView(Comment, db.session))
+
+path = join(dirname(__file__), 'uploads')
+admin.add_view(FileAdmin(path, '/uploads/', name='Uploads'))
+admin.add_view(NotificationsView(name='Notifications', endpoint='notify'))
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
