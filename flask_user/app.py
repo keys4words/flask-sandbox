2363fd6c3cd8941d7a70b5e08451bb3fe8e760e2
keys4words <keys4words@gmail.com>
2021-01-11 16:24:20 +0300

    add flask user

--- flask_user/app.py
+++ flask_user/app.py
@@ -2,21 +2,39 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_user import UserManager, UserMixin, SQLAlchemyAdapter, login_required
+from flask_mail import Mail
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.urandom(36)
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['CSRF_ENABLED'] = True
-app.config['USER_ENABLE_EMAIL'] = False
-
-
+app.config.from_pyfile('config.cfg')
 
 db = SQLAlchemy(app)
-manager = LoginManager(app)
+mail = Mail(app)
+
+#################################
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False, server_default='')
+    active = db.Column(db.Boolean(), nullable=False, server_default='0')
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    confirmed_at = db.Column(db.DateTime())
+
+#################################
+
+db_adapter = SQLAlchemyAdapter(db, User)
+user_manager = UserManager(db_adapter, app)
+
+@app.route('/')
+def index():
+    return 'This is unprotected home page!'
+
+
+@app.route('/profile')
+@login_required
+def profile():
+    return 'This is protected profile page'
 
 
 if __name__ == "__main__":
